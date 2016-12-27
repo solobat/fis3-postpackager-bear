184fd043192b830e57e15417838cd4a64d25917c
fengyang <fengyang1@le.com>
2016-12-27 13:40:42 +0800

    modify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var Combo = require('./lib/combo');
 
 module.exports = function (ret, conf, settings, opt) {
+    console.info('fis3-postpackager-bear run ...');
     // ret.src 所有的源码，结构是 {'<subpath>': <File 对象>}
     // ret.ids 所有源码列表，结构是 {'<id>': <File 对象>}
     // ret.map 如果是 spriter、postpackager 这时候已经能得到打包结果了，可以修改静态资源列表或者其他
@@ -9,22 +10,14 @@ module.exports = function (ret, conf, settings, opt) {
     var path = require('path')
     var root = fis.project.getProjectPath();
     var fs = require('fs');
-    var _ = fis.util;
-    var ns = fis.get('namespace');
-    var mapContent = ret.map;
-    var uriPre = fis.get('bearStatics');
-    uriPre = uriPre + (fis.get('projName') ? '/' + fis.get('projName') : '');
-    console.info(uriPre);
-    var regUriPre = new RegExp('^' + uriPre);
+    // var _ = fis.util;
 
+    var mapContent = ret.map;
     //根据配置，
     // 如果是pubType=pubvm时，移除 /page / 下面所有的配置
     // 如果是pubType=pubpage时，移除非 /page / 下面所有的配置
     if (settings && settings['pubType']) {
-        console.info(settings);
         var pubType = settings['pubType'];
-        var uriPre = settings['uriPre'];
-        // var mapContent = _.cloneDeep(mapContent);
         var res = mapContent['res'];
         switch (pubType) {
             case 'pubvm':
@@ -33,10 +26,6 @@ module.exports = function (ret, conf, settings, opt) {
                     if (propObj['extras'] && propObj['extras']['isPage']) {
                         //如果是页面类型，移除
                         delete res[k];
-                    } else {
-                        var uri = propObj['uri'];
-                        uri = uri.replace(regUriPre, '');
-                        propObj['uri'] = uri;
                     }
                 }
                 mapContent = res;
@@ -56,12 +45,15 @@ module.exports = function (ret, conf, settings, opt) {
         }
     }
 
+    //生成map.json
+    var ns = fis.get('namespace');
     var mapFile = ns ? (ns + '-map.json') : 'map.json';
     var map = fis.file.wrap(path.join(root, mapFile));
     map.setContent(JSON.stringify(mapContent, null, map.optimizer ? null : 4));
     ret.pkg[map.subpath] = map;
 
 
+    // url comb 处理
     var files = ret.src;
     var combSettings = settings['comb'];
     var combo = new Combo(combSettings);
